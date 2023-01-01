7be783c2a054ad5525a661b4dfcca00b51e6444f
Adarsh kumar kuiya <adarshk9889@gmail.com>
2023-01-02 00:43:22 +0530

    classroom

--- src/main/java/com/driver/StudentService.java
+++ src/main/java/com/driver/StudentService.java
@@ -1,8 +1,10 @@
 package com.driver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import java.util.*;
 @Service
+@Component
 public class StudentService {
     @Autowired
     StudentRepository student_repo;
